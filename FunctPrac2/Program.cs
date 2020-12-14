de6f863991caa1b7f8a7ccad59d49d3fcb7c6c23
starberdyev <starberdyev@mail.ru>
2020-12-15 01:53:55 +0300

    fix Task 1 add Task 2 ,3, 4

--- FunctPrac2/Program.cs
+++ FunctPrac2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FunctPrac2
 {
@@ -6,62 +7,161 @@ namespace FunctPrac2
     {
         static void Main(string[] args)
         {
-            Task1();
+            Task4();
         }
-        
+
         // Task 1
+
         private static void Task1()
         {
+            string[] str = Console.ReadLine().Split(" ");
+            Func<string[], int[]> parse = _string =>
             {
-                Action<int[]> addition = numerals =>
+                int[] ints = new int[_string.Length];
+                for (int i = 0; i < _string.Length; i++)
                 {
-                    for (var i = 0; i < numerals.Length; i++)
-                    {
-                        numerals[i]++;
-                    }
-                };
-                Action<int[]> multiplication = numerals =>
-                {
-                    for (var i = 0; i < numerals.Length; i++)
-                    {
-                        numerals[i] *= 2;
-                    }
-                };
-                Action<int[]> subtraction = numerals =>
-                {
-                    for (var i = 0; i < numerals.Length; i++)
-                    {
-                        numerals[i]--;
-                    }
-                };
-                Action<int[]> print = numerals => Console.WriteLine(string.Join(" ", numerals));
+                    ints[i] = Int32.Parse(_string[i]);
+                }
+                return ints;
+            };
 
-                Func<string, Action<int[]>> selector = command =>
+            int[] array = parse(str);
+            Func<int[], int[]> add = ints =>
+            {
+                for (int i = 0; i < ints.Length; i++)
                 {
-                    switch (command)
-                    {
-                        case "add":
-                            return addition;
-                        case "multiply":
-                            return multiplication;
-                        case "subtract":
-                            return subtraction;
-                        case "print":
-                            return print;
-                    }
+                    ints[i]++;
+                }
+                return ints;
+            };
 
-                    return _ => { };
-                };
-
-                var line = Console.ReadLine();
-                var strings = line.Split(' ');
-                var numbers = lineParser(strings);
-
-                while ((line = Console.ReadLine()) != "end")
+            Func<int[], int[]> multiply = ints =>
+            {
+                for (int i = 0; i < ints.Length; i++)
                 {
-                    selector(line)(numbers);
+                    ints[i] = ints[i] * 2;
+                }
+                return ints;
+            };
+
+            Func<int[], int[]> substract = ints =>
+            {
+                for (int i = 0; i < ints.Length; i++)
+                {
+                    ints[i]--;
+                }
+                return ints;
+            };
+
+            Action<int[]> print = ints =>
+            {
+                Console.WriteLine(string.Join(" ", ints));
+            };
+
+            string command = "";
+            while (command != "end")
+            {
+                command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "add":
+                        add(array);
+                        break;
+                    case "multiply":
+                        multiply(array);
+                        break;
+                    case "substract":
+                        substract(array);
+                        break;
+                    case "print":
+                        print(array);
+                        break;
                 }
             }
+        }
+
+        // Task 2
+        private static void Task2()
+        {
+            string[] str = Console.ReadLine().Split(" ");
+
+            List<int> listOfNumber = new List<int>();
+            for (int s = 0; s < str.Length; s++)
+            {
+                listOfNumber.Add(s);
+            }
+
+            Func<List<int>, List<int>> reverseAndRemove = ints =>
+            {
+                listOfNumber.Reverse();
+                int condition = Int32.Parse(Console.ReadLine());
+                for (int i = 0; i < ints.Count; i++)
+                {
+                    if (ints[i] % condition == 0)
+                    {
+                        ints.Remove(ints[i]);
+                        i--;
+                    }
+                }
+                return ints;
+            };
+
+            Console.WriteLine(string.Join(" ", reverseAndRemove(listOfNumber)));
+        }
+
+        // Task 3
+        private static void Task3()
+        {
+            int wordLength = Int32.Parse(Console.ReadLine());
+            string str = Console.ReadLine();
+
+            List<string> nameList = new List<string>(str.Split(" "));
+
+            Func<int, List<string>, List<string>> getName = (maxLength, strs) =>
+            {
+                for (int i = 0; i < strs.Count; i++)
+                {
+                    if (strs[i].Length > maxLength)
+                    {
+                        strs.Remove(strs[i]);
+                    }
+                }
+                return strs;
+            };
+
+            Console.WriteLine(string.Join(" ", getName(wordLength, nameList)));
+        }
+
+        // Task 4
+        private static void Task4()
+        {
+            string[] str = Console.ReadLine().Split(" ");
+
+            Func<string[], int[]> parse = _string =>
+            {
+                int[] numbers = new int[_string.Length];
+                for (int i = 0; i < _string.Length; i++)
+                {
+                    numbers[i] = Int32.Parse(_string[i]);
+                }
+
+                return numbers;
+            };
+
+            var comparer = Comparer<int>.Create((x, y) =>
+            {
+                if (x % 2 == 0 && y % 2 != 0)
+                    return -1;
+                if (x % 2 != 0 && y % 2 == 0)
+                    return 1;
+                return x.CompareTo(y);
+            });
+
+            var num = parse(str);
+            Array.Sort(num, comparer);
+
+            Console.WriteLine(string.Join(" ", num));
         }
     }
 }
