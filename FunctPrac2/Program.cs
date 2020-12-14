56b89b6fb77650bc07ccb8f5ee2c99f464d46ebe
starberdyev <starberdyev@mail.ru>
2020-12-14 10:38:07 +0300

    Добавьте файлы проекта.

---
+++ FunctPrac2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace FunctPrac2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+        }
+        
+        // Task 1
+        private static void Task1()
+        {
+            {
+                Action<int[]> addition = numerals =>
+                {
+                    for (var i = 0; i < numerals.Length; i++)
+                    {
+                        numerals[i]++;
+                    }
+                };
+                Action<int[]> multiplication = numerals =>
+                {
+                    for (var i = 0; i < numerals.Length; i++)
+                    {
+                        numerals[i] *= 2;
+                    }
+                };
+                Action<int[]> subtraction = numerals =>
+                {
+                    for (var i = 0; i < numerals.Length; i++)
+                    {
+                        numerals[i]--;
+                    }
+                };
+                Action<int[]> print = numerals => Console.WriteLine(string.Join(" ", numerals));
+
+                Func<string, Action<int[]>> selector = command =>
+                {
+                    switch (command)
+                    {
+                        case "add":
+                            return addition;
+                        case "multiply":
+                            return multiplication;
+                        case "subtract":
+                            return subtraction;
+                        case "print":
+                            return print;
+                    }
+
+                    return _ => { };
+                };
+
+                var line = Console.ReadLine();
+                var strings = line.Split(' ');
+                var numbers = lineParser(strings);
+
+                while ((line = Console.ReadLine()) != "end")
+                {
+                    selector(line)(numbers);
+                }
+            }
+        }
+    }
+}
